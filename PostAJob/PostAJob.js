61209a3e18be450f984b21c5ef2aebe305e389b1
Your Name <leobarrientos02@gmail.com>
2021-10-27 15:37:56 -0400

    Trying something different

--- PostAJob/PostAJob.js
+++ PostAJob/PostAJob.js
@@ -39,10 +39,10 @@ option2.addEventListener("click", () => {
 continueBtn.addEventListener("click", () => {
   if (choosen === 1) {
     //localStorage.setItem("TimePeriod", "Short Term");
-    window.location.href = "./postTitle.html";
+    window.location.href = "./postTitle.php";
   } else if (choosen === 2) {
     //localStorage.setItem("TimePeriod", "Long Term");
-    window.location.href = "./postTitle.html";
+    window.location.href = "./postTitle.php";
   } else {
     //localStorage.setItem("TimePeriod", null);
   }
