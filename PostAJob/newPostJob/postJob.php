101facfd9ce74a32c5b6b5186ade8c0b2ff7bd80
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-10-28 02:20:47 -0400

    update

--- PostAJob/newPostJob/postJob.php
+++ PostAJob/newPostJob/postJob.php
@@ -59,7 +59,7 @@ if (!isset($_SESSION['userid'])) {
                         }
 
                         //print_r($_SESSION['post']);
-                        $datePosted = date("m/d/y");
+                        $datePosted = date("Y-m-d");
                         $sql = "INSERT INTO jobs(length,title,skills,size,location,budget,rate,description,image,user_id,freelancer_id,status, datePosted) VALUES('$length','$title','None','$size','$location','$maxbudget','$hourrate', '$description', NULL, '$userID',0,0, '$datePosted')";
                         $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
