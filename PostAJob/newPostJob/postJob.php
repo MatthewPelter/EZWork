dfcdbeec8baedf2bf3cf5644eafbd82f5c15307f
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-10-28 00:11:54 -0400

    update

--- PostAJob/newPostJob/postJob.php
+++ PostAJob/newPostJob/postJob.php
@@ -80,21 +80,9 @@ if (!isset($_SESSION['userid'])) {
                     header("location: length.php"); // Redirecting to first page.
                 }
             } else {
-                if (empty($_SESSION['error_page6'])) {
-                    header("location: length.php"); // Redirecting to first page.
-                }
                 header("location: length.php"); // Redirecting to first page.
             }
             ?>
-
-            <span id="error">
-                <?php
-                if (!empty($_SESSION['error_page6'])) {
-                    echo $_SESSION['error_page6'];
-                    unset($_SESSION['error_page6']);
-                }
-                ?>
-            </span>
         </div>
     </div>
     <?php include '../../footer.php'; ?>
