95a58e9362f3cf5872ac3431be4ff3fe2d890c35
Your Name <leobarrientos02@gmail.com>
2021-10-30 03:50:46 -0400

    Sylting post  job

--- PostAJob/newPostJob/length.php
+++ PostAJob/newPostJob/length.php
@@ -69,7 +69,7 @@ if (!isset($_SESSION['userid'])) {
                 }
                 ?>
             </span>
-            <form action="postTitle.php" method="post">
+            <form action="postTitle.php" method="post" style="width: 100%;">
                 <h3>Choose a Job Length:<span>*</span></h3>
                 <div class="lengthContainers">
                     <div class="short">
