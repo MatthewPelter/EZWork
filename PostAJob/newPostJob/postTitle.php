4faf0f3e449e51e41b6056ffdb1b94f8dfe27e77
Your Name <leobarrientos02@gmail.com>
2021-10-31 22:07:33 -0400

    Sylting post  job

--- PostAJob/newPostJob/postTitle.php
+++ PostAJob/newPostJob/postTitle.php
@@ -17,49 +17,188 @@ if (isset($_POST['length'])) {
     }
 }
 ?>
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <title>Post a Job</title>
-    <link rel="stylesheet" href="../../Styles/style.css" />
-</head>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
+            <title>EZWork | Find Jobs or Freelancers</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet"> 
+            <link rel="icon" href="../logo/logo.svg">
+            <link rel="stylesheet" href="../../Styles/style.css">
+        </head>
+    </head>
 
 <body>
     <?php include '../../navbar.php'; ?>
-    <div class="PostAJob">
-        <div class="PostAJobContainer">
-            <h2>Post a Job</h2>
-            <hr />
-            <span id="error">
-                <?php
-                // To show error of page 2.
-                if (!empty($_SESSION['error_page2'])) {
-                    echo $_SESSION['error_page2'];
-                    unset($_SESSION['error_page2']);
-                }
-                ?>
-            </span>
-            <form action="scope.php" method="post">
-                <label>Write a title for your job post :<span>*</span></label><br />
-                <input name="title" id="title" type="text" placeholder="I need my garage door installed..." value=""><br />
-                <label>Description :<span>*</span></label><br />
-                <input name="description" id="description" type="text" placeholder="My son drove into my garage door and completely ruined my beautiful door..." value="">
 
-                <br />
-                <input type="reset" value="Reset" />
 
-                <input type="submit" value="Next" />
+        <!--Post A Job More Details-->
+        <div class="postJob-detail">
+        <div class="detail-container">
+            <div class="detail-progress-section">
+                <div class="progressBar">
+                    <progress id="jobPostProgress" value="25" max="100"></progress>
+                    <ul>
+                        <li id="current">Length</li>
+                        <li id="current">Title</li>
+                        <li>Scope</li>
+                        <li>Location</li>
+                        <li>Budget</li>
+                    </ul>
+                </div>
+                <div class="title-card-intro">
+                    <h2>Let's add a strong title and description.</h2>
+                    <p>A strong, detailed title would stand out to the right freelancers. It's the first thing they would see. Be very descriptive</p>
+                </div>
+            </div>
+            <div class="detail-input-section">
+                <span id="error">
+                    <?php
+                    // To show error of page 2.
+                    if (!empty($_SESSION['error_page2'])) {
+                        echo $_SESSION['error_page2'];
+                        unset($_SESSION['error_page2']);
+                    }
+                    ?>
+                </span>
+            <form action="scope.php" method="post" style="width: 100%;">
+                <h3>Write a title for your job post</h3>
+                <div class="titleContainer">
+                    <input name="title" id="title" type="text" placeholder="I need my garage door installed..." value=""><br />
+                </div>
+                <h4>Example titles</h4>
+                <ul>
+                    <li>Build a website for my local coffee shop business</li>
+                    <li>Logo Designer needed to design a modern company logo.</li>
+                    <li>Computer Technician needed to build my custom gaming pc.</li>
+                </ul>
+                <h3>Enter a description of your job post</h3>
+                <div class="descriptionContainer">
+                    <input name="description" id="description" type="text" placeholder="My son drove into my garage door and completely ruined my beautiful door..." value="">
+                </div>
+                <div class="CancelOrNext">
+                    <input type="reset" value="Reset" />
+
+                    <input type="submit" value="Next" />
+                </div>
             </form>
+
+            </div>
         </div>
     </div>
+    <!--Post A Job End-->
 
     <?php include '../../footer.php'; ?>
+
+    <!--DataList-->
+    <datalist id="allskills"></datalist>
+
 </body>
-<script src="../../ClientProfile/app.js"></script>
+<!--Nav bar script-->
+<script>
+    var job = document.querySelector('.jobCard');
+    var talent = document.querySelector('.talentCard');
+    var project = document.querySelector('.projectCard');
+    var help = document.querySelector('.helpCard');
+    var session = document.querySelector('.sessionCard');
+    function toggleJob(){
+        var job = document.querySelector('.jobCard');
+        if(job.style.display === 'none'){
+            job.style.display = 'inline-block';
+            talent.style.display = 'none';
+            project.style.display = 'none';
+            help.style.display = 'none';
+            session.style.display = 'none';
+        }
+        else{
+            job.style.display='none';
+            
+        }
+    }
+    function toggleTalent(){
+        var talent = document.querySelector('.talentCard');
+        if(talent.style.display==='none'){
+            talent.style.display = 'inline-block';
+            job.style.display = 'none';
+            project.style.display = 'none';
+            help.style.display = 'none';
+            session.style.display = 'none';
+        }
+        else{
+            talent.style.display = 'none';
+        }
+    }
+    function toggleProject(){
+        var project = document.querySelector('.projectCard');
+        if(project.style.display==='none'){
+            project.style.display = 'inline-block';
+            talent.style.display = 'none';
+            job.style.display = 'none';
+            help.style.display = 'none';
+            session.style.display = 'none';
+        }
+        else{
+            project.style.display = 'none';
+        }
+    }
+    function toggleHelp(){
+        var help = document.querySelector('.helpCard');
+        if(help.style.display==='none'){
+            help.style.display = 'inline-block';
+            talent.style.display = 'none';
+            project.style.display = 'none';
+            job.style.display = 'none';
+            session.style.display = 'none';
+        }
+        else{
+            help.style.display = 'none';
+        }
+    }
+    function toggleSession(){
+       
+        if(session.style.display==='none'){
+            session.style.display = 'inline-block';
+            talent.style.display = 'none';
+            project.style.display = 'none';
+            help.style.display = 'none';
+            job.style.display = 'none';
+        }
+        else{
+            session.style.display = 'none';
+        }
+    }
 
+</script>
+<!--Toggle the nav burger button-->
+<script>
+    const navIcon = document.getElementById("nav-burger");
+    const profileMobileNav = document.querySelector(".profile-mobile-nav");
+
+    function myFunction(x) {
+        x.classList.toggle("change");
+        if(x.classList.contains('change')){
+            profileMobileNav.style.display = "inline-block";
+            searchIcon.style.opacity='0';
+        }
+        else{
+            profileMobileNav.style.display='none';
+            searchIcon.style.opacity='1';
+        }
+    }
+
+</script>
 </html>
-
 
 <?php
 /* We can use this for the registration to improve input validation
@@ -85,3 +224,17 @@ $_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
  }
  */
 ?>
+
+<div class="PostAJob">
+    <div class="PostAJobContainer">
+        <h2>Post a Job</h2>
+        <hr />
+
+        <form action="scope.php" method="post">
+            <label>Write a title for your job post :<span>*</span></label><br />
+
+            <br />
+
+        </form>
+    </div>
+</div>
