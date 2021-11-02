7d86549a02dc3067332008279324443865e68a72
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-02 03:39:17 -0400

    update

--- classes/Mail.php
+++ classes/Mail.php
@@ -38,7 +38,6 @@ class Mail
                         $mail->Body = $body;
 
                         $mail->send();
-                        echo 'Message has been sent';
                 } catch (Exception $e) {
                         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
                 }
