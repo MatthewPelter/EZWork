d75ffe055bffcd8f7f35223038f805dc1b610520
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-02 01:42:10 -0400

    update

--- components/settings-change-password.php
+++ components/settings-change-password.php
@@ -3,36 +3,40 @@ session_start();
 require_once("../classes/DB.php");
 include './classes/Mail.php';
 
-if (isset($_POST['currentPassword']) && isset($_POST['password']) && isset($_POST['password2'])) {
+function securityscan($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if (isset($_POST['currentPassword']) && isset($_POST['password']) && isset($_POST['password2']) && !empty($_POST['currentPassword']) && !empty($_POST['password']) && !empty($_POST['password2'])) {
     $username = $_SESSION['userid'];
-    $password = $_POST['currentPassword'];
-    $passwordNew = $_POST['password'];
-    $passwordNew2 = $_POST['password2'];
+    $password = securityscan($_POST['currentPassword']);
+    $passwordNew = securityscan($_POST['password']);
+    $passwordNew2 = securityscan($_POST['password2']);
 
-    if (empty($password) || empty($passwordNew) || empty($passwordNew2)) {
-        echo "Required Fields are Empty...";
-    } else {
-        $query = "SELECT * FROM clients WHERE username='$username'";
-        $result = mysqli_query($conn, $query);
-        $row = mysqli_fetch_assoc($result);
-        if (password_verify($password, $row['password'])) {
-            if ($passwordNew == $passwordNew2) {
-                $passwordNew = password_hash($passwordNew, PASSWORD_BCRYPT);
-                mysqli_query($conn, "UPDATE clients SET password = '$passwordNew'");
-                echo "Password has been reset successfully!";
+    $query = "SELECT * FROM clients WHERE username='$username'";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+    if (password_verify($password, $row['password'])) {
+        if ($passwordNew == $passwordNew2) {
+            $passwordNew = password_hash($passwordNew, PASSWORD_BCRYPT);
+            mysqli_query($conn, "UPDATE clients SET password = '$passwordNew'");
+            echo "Password has been reset successfully!";
 
-                $subject = 'Password was Reset!';
-                ob_start();
-                include 'changedPassEmail.phtml';
-                $body = ob_get_clean();
-                Mail::sendMail($subject, $body, $row['email']);
-            } else {
-                echo "Passwords do not match! Try again";
-            }
+            $subject = 'Password was Reset!';
+            ob_start();
+            include 'changedPassEmail.phtml';
+            $body = ob_get_clean();
+            Mail::sendMail($subject, $body, $row['email']);
         } else {
-            echo "Password is incorrect!";
+            echo "Passwords do not match! Try again";
         }
+    } else {
+        echo "Password is incorrect!";
     }
 } else {
-    echo "Error: Post variables not set...";
+    echo "Error: Post variables not set or empty...";
 }
