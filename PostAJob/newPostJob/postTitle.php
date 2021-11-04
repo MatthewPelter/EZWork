4fc5857a3a14d452231f168a1feb1b581d3c792d
Your Name <leobarrientos02@gmail.com>
2021-11-04 16:03:54 -0400

    Styling

--- PostAJob/newPostJob/postTitle.php
+++ PostAJob/newPostJob/postTitle.php
@@ -17,92 +17,91 @@ if (isset($_POST['length'])) {
     }
 }
 ?>
-</html>
 <!DOCTYPE html>
 <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
-            <title>EZWork | Find Jobs or Freelancers</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
+    <title>EZWork | Find Jobs or Freelancers</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet"> 
-            <link rel="icon" href="../logo/logo.svg">
-            <link rel="stylesheet" href="../../Styles/style.css">
-        </head>
-
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet"> 
+    <link rel="icon" href="../logo/logo.svg">
+    <link rel="stylesheet" href="../../Styles/style.css">
+</head>
 <body>
+
     <?php include '../../navbar.php'; ?>
 
-
-        <!--Post A Job More Details-->
-        <div class="postJob-detail-title">
-        <div class="detail-container">
-            <div class="detail-progress-section">
-                <div class="progressBar">
-                    <progress id="jobPostProgress" value="25" max="100"></progress>
+           <!--Post A Job More Details-->
+           <div class="postJob-detail-title">
+            <div class="detail-container">
+                <div class="detail-progress-section">
+                    <div class="progressBar">
+                        <progress id="jobPostProgress" value="25" max="100"></progress>
+                        <ul>
+                            <li id="current">Length</li>
+                            <li id="current">Title</li>
+                            <li>Scope</li>
+                            <li>Location</li>
+                            <li>Budget</li>
+                        </ul>
+                    </div>
+                    <div class="title-card-intro">
+                        <h2>Let's add a strong title and description.</h2>
+                        <p>A strong, detailed title would stand out to the right freelancers. It's the first thing they would see. Be very descriptive</p>
+                    </div>
+                </div>
+                <div class="detail-input-section">
+                <span id="error" style="color: red;padding: 0;">
+                        <?php
+                        // To show error of page 2.
+                        if (!empty($_SESSION['error_page2'])) {
+                            echo $_SESSION['error_page2'];
+                            unset($_SESSION['error_page2']);
+                        }
+                        ?>
+                </span>
+                <form action="scope.php" method="post" style="width: 100%;">
+                    <h3>Write a title for your job post</h3>
+                    <div class="titleContainer">
+                        <input name="title" id="title" type="text" placeholder="I need my garage door installed..." value=""><br />
+                    </div>
+                    <h4>Example titles</h4>
                     <ul>
-                        <li id="current">Length</li>
-                        <li id="current">Title</li>
-                        <li>Scope</li>
-                        <li>Location</li>
-                        <li>Budget</li>
+                        <li>Build a website for my local coffee shop business</li>
+                        <li>Logo Designer needed to design a modern company logo.</li>
+                        <li>Computer Technician needed to build my custom gaming pc.</li>
                     </ul>
-                </div>
-                <div class="title-card-intro">
-                    <h2>Let's add a strong title and description.</h2>
-                    <p>A strong, detailed title would stand out to the right freelancers. It's the first thing they would see. Be very descriptive</p>
+                    <h3>Enter a job description</h3>
+                    <div class="descriptionContainer">
+                        <input name="description" id="description" type="text" placeholder="My son drove into my garage door and completely ruined my beautiful door..." value="">
+                    </div>
+                    <div class="CancelOrNext">
+                        <input type="reset" value="Reset" id="reset" />
+    
+                        <input type="submit" value="Next" id="nextScope"/>
+                    </div>
+                </form>
+    
                 </div>
             </div>
-            <div class="detail-input-section">
-            <span id="error" style="color: red;padding: 0;">
-                    <?php
-                    // To show error of page 2.
-                    if (!empty($_SESSION['error_page2'])) {
-                        echo $_SESSION['error_page2'];
-                        unset($_SESSION['error_page2']);
-                    }
-                    ?>
-            </span>
-            <form action="scope.php" method="post" style="width: 100%;">
-                <h3>Write a title for your job post</h3>
-                <div class="titleContainer">
-                    <input name="title" id="title" type="text" placeholder="I need my garage door installed..." value=""><br />
-                </div>
-                <h4>Example titles</h4>
-                <ul>
-                    <li>Build a website for my local coffee shop business</li>
-                    <li>Logo Designer needed to design a modern company logo.</li>
-                    <li>Computer Technician needed to build my custom gaming pc.</li>
-                </ul>
-                <h3>Enter a job description</h3>
-                <div class="descriptionContainer">
-                    <input name="description" id="description" type="text" placeholder="My son drove into my garage door and completely ruined my beautiful door..." value="">
-                </div>
-                <div class="CancelOrNext">
-                    <input type="reset" value="Reset" id="reset" />
-
-                    <input type="submit" value="Next" id="nextScope"/>
-                </div>
-            </form>
-
             </div>
-        </div>
-    </div>
-    <!--Post A Job End-->
+            <!--Post A Job End-->
 
-    <?php include '../../footer.php'; ?>
+            <?php include '../../footer.php'; ?>
 
-    <!--DataList-->
-    <datalist id="allskills"></datalist>
-
+            <!--DataList-->
+            <datalist id="allskills"></datalist>
+    
 </body>
+
 <!--Nav bar script-->
 <script>
     var job = document.querySelector('.jobCard');
@@ -196,8 +195,6 @@ if (isset($_POST['length'])) {
     }
 
 </script>
-</html>
-
 <?php
 /* We can use this for the registration to improve input validation
 
