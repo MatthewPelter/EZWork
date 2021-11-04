0a2a8991a3e978020a1aee12e73108d861859888
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-04 01:09:10 -0400

    update

--- Profile/userprofile.php
+++ Profile/userprofile.php
@@ -640,6 +640,23 @@ if (mysqli_num_rows($result) > 0) {
 <script src="../SkillsContainer/searchProfile.js"></script>
 <script src="./app.js"></script>
 <script>
+    var messageBox = document.querySelector('.js-message');
+    var btn = document.querySelector('.js-message-btn');
+    var card = document.querySelector('.js-profile-card');
+    var closeBtn = document.querySelectorAll('.js-message-close');
+
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        card.classList.add('active');
+    });
+
+    closeBtn.forEach(function(element, index) {
+        console.log(element);
+        element.addEventListener('click', function(e) {
+            e.preventDefault();
+            card.classList.remove('active');
+        });
+    });
     var job = document.querySelector('.jobCard');
     var talent = document.querySelector('.talentCard');
     var project = document.querySelector('.projectCard');
