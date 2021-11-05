5414958f600660401ca64296d84f5775e790e33e
Your Name <leobarrientos02@gmail.com>
2021-11-05 18:30:22 -0400

    styling postBudget.php

--- PostAJob/newPostJob/budget.php
+++ PostAJob/newPostJob/budget.php
@@ -18,20 +18,53 @@ if (isset($_POST['location'])) {
     }
 }
 ?>
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <title>Post a Job</title>
-    <link rel="stylesheet" href="../../Styles/style.css" />
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
+        <title>EZWork | Find Jobs or Freelancers</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+        <link rel="icon" href="../../logo/logo.svg">
+        <link rel="stylesheet" href="../../Styles/style.css">
+    </head>
 </head>
 
 <body>
+
     <?php include '../../navbar.php'; ?>
-    <div class="PostAJob">
-        <div class="PostAJobContainer">
-            <h2>Post a Job</h2>
-            <hr />
+
+    <!--Post A Job More Details-->
+    <div class="postJob-detail-budget">
+        <div class="detail-budget-container">
+            <div class="detail-progress-section">
+                <div class="progressBar">
+                    <progress id="jobPostProgress" value="100" max="100"></progress>
+                    <ul>
+                        <li id="current">Length</li>
+                        <li id="current">Title</li>
+                        <li id="current">Scope</li>
+                        <li id="current">Location</li>
+                        <li id="current">Budget</li>
+                    </ul>
+                </div>
+                <div class="title-card-intro">
+                    <h2>Tell us about your budget.</h2>
+                    <p>This will help us match you to talent within your range.</p>
+                </div>
+            </div>
+            <div class="detail-input-section">
             <span id="error">
                 <?php
                 if (!empty($_SESSION['error_page5'])) {
@@ -41,8 +74,6 @@ if (isset($_POST['location'])) {
                 ?>
             </span>
             <form action="postJob.php" method="post">
-                <b>Select your preferred freelance location :</b>
-
                 <input type="radio" id="rate" name="budgetoption" value="rate" required>
                 <label for="rate">Hourly Rate</label>
                 <input type="radio" id="budget" name="budgetoption" value="budget">
@@ -60,11 +91,21 @@ if (isset($_POST['location'])) {
                     <input name="maxbudget" id="maxbudget" type="number" placeholder="Enter $ Amount" value="">
                 </div>
 
-                <input type="submit" value="Next" />
+                <div class="CancelOrNext">
+                    <input type="submit" value="Post Job" id="JobPost" />
+                </div>
+
             </form>
+            </div>
         </div>
     </div>
+    <!--Post A Job End-->
+
     <?php include '../../footer.php'; ?>
+
+    <!--DataList-->
+    <datalist id="allskills"></datalist>
+
 </body>
 
 <script src="../../ClientProfile/app.js"></script>
