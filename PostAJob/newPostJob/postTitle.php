10c3c92fe1bcf3f109e3ecf5f16f514bf1110912
Your Name <leobarrientos02@gmail.com>
2021-11-05 04:08:56 -0400

    trying new grid design for profile page

--- PostAJob/newPostJob/postTitle.php
+++ PostAJob/newPostJob/postTitle.php
@@ -22,7 +22,6 @@ if (isset($_POST['length'])) {
 <html lang="en">
 
 <head>
-<<<<<<< HEAD
 
     <head>
         <meta charset="UTF-8">
@@ -42,23 +41,6 @@ if (isset($_POST['length'])) {
     </head>
 </head>
 
-=======
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
-    <title>EZWork | Find Jobs or Freelancers</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet"> 
-    <link rel="icon" href="../logo/logo.svg">
-    <link rel="stylesheet" href="../../Styles/style.css">
-</head>
->>>>>>> e19a46fff3759f9834e4869780b4abe2241f2f88
 <body>
 
     <?php include '../../navbar.php'; ?>
