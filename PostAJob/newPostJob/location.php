ee4446183ea0b7ab995b9e31325cd507307640e0
Your Name <leobarrientos02@gmail.com>
2021-11-05 13:40:28 -0400

    styling postLocation.php

--- PostAJob/newPostJob/location.php
+++ PostAJob/newPostJob/location.php
@@ -18,20 +18,53 @@ if (isset($_POST['size'])) {
     }
 }
 ?>
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <title>Post a Job</title>
-    <link rel="stylesheet" href="../../Styles/style.css" />
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
+        <title>EZWork | Find Jobs or Freelancers</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+        <link rel="icon" href="../../logo/logo.svg">
+        <link rel="stylesheet" href="../../Styles/style.css">
+    </head>
 </head>
 
 <body>
+
     <?php include '../../navbar.php'; ?>
-    <div class="PostAJob">
-        <div class="PostAJobContainer">
-            <h2>Post a Job</h2>
-            <hr />
+
+    <!--Post A Job More Details-->
+    <div class="postJob-detail-location">
+        <div class="detail-location-container">
+            <div class="detail-progress-section">
+                <div class="progressBar">
+                    <progress id="jobPostProgress" value="75" max="100"></progress>
+                    <ul>
+                        <li id="current">Length</li>
+                        <li id="current">Title</li>
+                        <li id="current">Scope</li>
+                        <li id="current">Location</li>
+                        <li>Budget</li>
+                    </ul>
+                </div>
+                <div class="title-card-intro">
+                    <h2>Select your preferred freelance location.</h2>
+                    <p>This increases proposals from talent in a specific region, but still opens your job post to all candidates.</p>
+                </div>
+            </div>
+            <div class="detail-input-section">
             <span id="error">
                 <?php
                 if (!empty($_SESSION['error_page4'])) {
@@ -41,19 +74,116 @@ if (isset($_POST['size'])) {
                 ?>
             </span>
             <form action="budget.php" method="post">
-                <b>Select your preferred freelance location :</b>
-
                 <input type="radio" id="us" name="location" value="us" required>
-                <label for="us">U.S. only</label><br>
+                <label for="us">U.S. only</label>
                 <input type="radio" id="world" name="location" value="world">
-                <label for="world">Worldwide</label><br>
+                <label for="world">Worldwide</label>
 
-                <input type="submit" value="Next" />
+                <div class="CancelOrNext">
+                    <input type="submit" value="Next: Budget" id="nextBudget"/>
+                </div>
             </form>
+            </div>
         </div>
     </div>
+    <!--Post A Job End-->
+
     <?php include '../../footer.php'; ?>
+
+    <!--DataList-->
+    <datalist id="allskills"></datalist>
+
 </body>
-<script src="../../ClientProfile/app.js"></script>
+
+<!--Nav bar script-->
+<script>
+    var job = document.querySelector('.jobCard');
+    var talent = document.querySelector('.talentCard');
+    var project = document.querySelector('.projectCard');
+    var help = document.querySelector('.helpCard');
+    var session = document.querySelector('.sessionCard');
+
+    function toggleJob() {
+        var job = document.querySelector('.jobCard');
+        if (job.style.display === 'none') {
+            job.style.display = 'inline-block';
+            talent.style.display = 'none';
+            project.style.display = 'none';
+            help.style.display = 'none';
+            session.style.display = 'none';
+        } else {
+            job.style.display = 'none';
+
+        }
+    }
+
+    function toggleTalent() {
+        var talent = document.querySelector('.talentCard');
+        if (talent.style.display === 'none') {
+            talent.style.display = 'inline-block';
+            job.style.display = 'none';
+            project.style.display = 'none';
+            help.style.display = 'none';
+            session.style.display = 'none';
+        } else {
+            talent.style.display = 'none';
+        }
+    }
+
+    function toggleProject() {
+        var project = document.querySelector('.projectCard');
+        if (project.style.display === 'none') {
+            project.style.display = 'inline-block';
+            talent.style.display = 'none';
+            job.style.display = 'none';
+            help.style.display = 'none';
+            session.style.display = 'none';
+        } else {
+            project.style.display = 'none';
+        }
+    }
+
+    function toggleHelp() {
+        var help = document.querySelector('.helpCard');
+        if (help.style.display === 'none') {
+            help.style.display = 'inline-block';
+            talent.style.display = 'none';
+            project.style.display = 'none';
+            job.style.display = 'none';
+            session.style.display = 'none';
+        } else {
+            help.style.display = 'none';
+        }
+    }
+
+    function toggleSession() {
+
+        if (session.style.display === 'none') {
+            session.style.display = 'inline-block';
+            talent.style.display = 'none';
+            project.style.display = 'none';
+            help.style.display = 'none';
+            job.style.display = 'none';
+        } else {
+            session.style.display = 'none';
+        }
+    }
+</script>
+<!--Toggle the nav burger button-->
+<script>
+    const navIcon = document.getElementById("nav-burger");
+    const profileMobileNav = document.querySelector(".profile-mobile-nav");
+
+    function myFunction(x) {
+        x.classList.toggle("change");
+        if (x.classList.contains('change')) {
+            profileMobileNav.style.display = "inline-block";
+            searchIcon.style.opacity = '0';
+        } else {
+            profileMobileNav.style.display = 'none';
+            searchIcon.style.opacity = '1';
+        }
+    }
+</script>
 
 </html>
