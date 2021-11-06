889ecee5fd38ef23531e789bff4c3604000dbb3a
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-06 00:01:02 -0400

    update

--- api/delete-post.php
+++ api/delete-post.php
@@ -15,12 +15,17 @@ if (isset($_POST['postID']) && isset($_SESSION['user_id'])) {
     $jobid = securityscan($_POST['postID']);
     $jobCheck = mysqli_query($conn, "SELECT user_id FROM jobs WHERE id='$jobid");
     $userid = mysqli_fetch_assoc($jobCheck);
-    $userid = $userid['user_id'];
-    if ($_SESSION['user_id'] == $userid) {
-        mysqli_query($conn, "DELETE FROM jobs WHERE id='$jobid") or die(mysqli_errno($conn));
-        echo "Job deleted Successfully!";
+    $user_id = $userid['user_id'];
+
+    if (mysqli_num_rows($jobCheck) > 0) {
+        if ($_SESSION['user_id'] == $user_id) {
+            mysqli_query($conn, "DELETE FROM jobs WHERE id='$jobid") or die(mysqli_errno($conn));
+            echo "Job deleted Successfully!";
+        } else {
+            echo "This is not your post...";
+        }
     } else {
-        echo "This is not your post...";
+        echo "Job doesn't Exist";
     }
 } else {
     echo "Error: Somthing went wrong :(";
