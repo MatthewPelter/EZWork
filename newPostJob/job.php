70f496e83d4fe0218c2e7b5755c01a95177b469b
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-09 15:14:16 -0500

    update

--- newPostJob/job.php
+++ newPostJob/job.php
@@ -114,7 +114,7 @@ if (mysqli_num_rows($jobResult) > 0) {
 
                     </div>
                     <div class="experience">
-                        <h4>Entry Level</h4>
+                        <h4><?php echo $r['experience']; ?></h4>
                     </div>
                 </div>
 
