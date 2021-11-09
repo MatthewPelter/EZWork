dd7f506743cb1f76da5c53ea18308a912b0b15be
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-09 02:53:07 -0500

    update

--- newPostJob/scope.php
+++ newPostJob/scope.php
@@ -18,29 +18,29 @@ if (isset($_POST['skills'])) {
         $img = $_FILES['image'];
         $filename = $img['tmp_name'];
         $client_id = "9f482e3edae002b";
-        $handle = fopen($filename, "r");
-        // $data = fread($handle, filesize($filename));
-        // $pvars   = array('image' => base64_encode($data));
-        // $timeout = 30;
-        // $curl = curl_init();
-        // curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image/');
-        // curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
-        // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
-        // curl_setopt($curl, CURLOPT_POST, 1);
-        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        // curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-        // $out = curl_exec($curl);
-        // curl_close($curl);
-        // $pms = json_decode($out, true);
-        // $url = $pms['data']['link'];
-        // if ($url != "") {
-        //     // echo "<h2>Uploaded Without Any Problem</h2>";
-        //     // echo "<img src='$url'/>";
-        //     $_POST['image'] = $url;
-        // } else {
-        //     echo "<h2>There's a Problem</h2>";
-        //     echo $pms['data']['error'];
-        // }
+        $handle = fopen($filename, "r") or die("Error opening file");
+        $data = fread($handle, filesize($filename));
+        $pvars   = array('image' => base64_encode($data));
+        $timeout = 30;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image/');
+        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+        $out = curl_exec($curl);
+        curl_close($curl);
+        $pms = json_decode($out, true);
+        $url = $pms['data']['link'];
+        if ($url != "") {
+            // echo "<h2>Uploaded Without Any Problem</h2>";
+            // echo "<img src='$url'/>";
+            $_POST['image'] = $url;
+        } else {
+            echo "<h2>There's a Problem</h2>";
+            echo $pms['data']['error'];
+        }
 
         foreach ($_POST as $key => $value) {
             $_SESSION['post'][$key] = $value;
