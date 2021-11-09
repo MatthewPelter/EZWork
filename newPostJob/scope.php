141d324a4e0d03ebb1f7811de97eac16753d9e97
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-09 02:47:17 -0500

    update

--- newPostJob/scope.php
+++ newPostJob/scope.php
@@ -23,24 +23,24 @@ if (isset($_POST['skills'])) {
         $pvars   = array('image' => base64_encode($data));
         $timeout = 30;
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image/');
-        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-        $out = curl_exec($curl);
-        curl_close($curl);
-        $pms = json_decode($out, true);
-        $url = $pms['data']['link'];
-        if ($url != "") {
-            // echo "<h2>Uploaded Without Any Problem</h2>";
-            // echo "<img src='$url'/>";
-            $_POST['image'] = $url;
-        } else {
-            echo "<h2>There's a Problem</h2>";
-            die($pms['data']['error']);
-        }
+        // curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image/');
+        // curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+        // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+        // curl_setopt($curl, CURLOPT_POST, 1);
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+        // $out = curl_exec($curl);
+        // curl_close($curl);
+        // $pms = json_decode($out, true);
+        // $url = $pms['data']['link'];
+        // if ($url != "") {
+        //     // echo "<h2>Uploaded Without Any Problem</h2>";
+        //     // echo "<img src='$url'/>";
+        //     $_POST['image'] = $url;
+        // } else {
+        //     echo "<h2>There's a Problem</h2>";
+        //     echo $pms['data']['error'];
+        // }
 
         foreach ($_POST as $key => $value) {
             $_SESSION['post'][$key] = $value;
