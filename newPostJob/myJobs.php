4a106b2e42769c015cff7e772a6ac346991f3a53
Your Name <leobarrientos02@gmail.com>
2021-11-10 16:41:57 -0500

    Adding a myJobs page, media query

--- newPostJob/myJobs.php
+++ newPostJob/myJobs.php
@@ -58,15 +58,27 @@ $userID = $_SESSION['user_id'];
             </div>
 
             <div class="postedJob">
-                <span>
                     <?php
                     $jobSQL = "SELECT * FROM jobs WHERE user_id='$userID' ORDER BY id DESC";
                     $jobResult = mysqli_query($conn, $jobSQL) or die(mysqli_errno($conn));
                     if (mysqli_num_rows($jobResult) == 0) {
+
+                    ?>
+                    <span>
+                        <?php
                         echo "You currently have no job postings listed.";
+                        ?>
+                    </span>
+                    <?php
+                    }
+                    else{
+                    
+                    ?>
+                    <span style="display: none;">
+                    </span>
+                    <?php
                     }
                     ?>
-                </span>
                 <?php
                 if (mysqli_num_rows($jobResult) > 0) {
                     while ($r = mysqli_fetch_assoc($jobResult)) {
