6237e346eb0a770ad649f61890b5ce61b116120a
Your Name <leobarrientos02@gmail.com>
2021-11-15 06:00:25 -0500

    Adding the filter section to the allJobs.php page

--- newPostJob/jobs.php
+++ newPostJob/jobs.php
@@ -337,21 +337,7 @@ $jobsQuery = mysqli_query($conn, $jobsSQL);
 
     </datalist>
 </body>
-<script>
-function toggleSearch2(){
-    var search2 = document.getElementById('search2');
-    var profileMobileNav2 = document.querySelector(".profile-mobile-nav");
-    if(getComputedStyle(profileMobileNav2).display === "inline-block"){
-        search2.style.display = "none";
-    }
-    else if(getComputedStyle(profileMobileNav2).display === "none"){
-        search2.style.display = "inline-block";
-    }
-}
 
-window.onload = toggleSearch2();
-
-</script>
 <!--Script for the search bar and datalist-->
 <script src="../SkillsContainer/allJobsSkills.js"></script>
 
