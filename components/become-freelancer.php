e6b1bc9d82b09238251f6973f9b70dc7a584e5ff
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-18 04:43:01 -0500

    update

--- components/become-freelancer.php
+++ components/become-freelancer.php
@@ -48,7 +48,7 @@ if (isset($_POST['submit'])) {
     $createFreelancer = mysqli_query(
         $conn,
         "INSERT INTO freelancers(linkedin, user_id, expertise, experience, school, degree, fos, schoolStart, schoolEnd, jobTitle, company, jobLocation, jobStart, jobEnd, hourRate, description, country, street, apt, city, state, zip) VALUES ($linkedin, $user_id, $expertise, $experience, $school, $degree, $fos, $schoolStart, $schoolEnd, $jobTitle, $company, $jobLocation, $jobStart, $jobEnd, $hourRate, $description, $country, $street, $apt, $city, $state, $zip)"
-    );
+    ) or die(mysqli_errno($conn));
     if ($createFreelancer) {
         $sql = "SELECT id FROM freelancers WHERE user_id = '$user_id'";
         $result = mysqli_query($conn, $sql);
@@ -61,6 +61,4 @@ if (isset($_POST['submit'])) {
     } else {
         echo "could not insert into database";
     }
-} else {
-    echo "submit not set";
 }
