088b4f0dbd1ad7939b6562568c8e418e75ba20ad
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-22 17:14:22 -0500

    update

--- newPostJob/contracts.php
+++ newPostJob/contracts.php
@@ -1,5 +1,5 @@
 <?php
-/*session_start();
+session_start();
 include '../components/session-checker.php';
 require_once("../classes/DB.php");
 
@@ -120,5 +120,3 @@ if ($checkFreelancer == NULL) {
 </body>
 
 </html>
-
-<?php */
