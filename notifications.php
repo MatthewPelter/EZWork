254b7f2bf01fd5435a7be8db1cc7dc9b092fe6a4
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-27 02:28:22 -0500

    update

--- notifications.php
+++ notifications.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
-include("./components/session-checker.php");
+include './components/session-checker.php';
+require_once("./classes/DB.php");
 
 echo "Here are your notifications:";
