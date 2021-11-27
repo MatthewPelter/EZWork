3263574b6a45aaaa681e85d23dde4f22cfc54ec2
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-27 02:39:14 -0500

    update

--- notifications.php
+++ notifications.php
@@ -40,7 +40,7 @@ echo "Here are your notifications:";
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
-        <link rel="icon" href="../logo/logo.svg">
+        <link rel="icon" href="./logo/logo.svg">
         <link rel="stylesheet" href="./Styles/style.css">
     </head>
 </head>
