8b7d03484357010131eb136a75c852bf82efb44a
Your Name <leobarrientos02@gmail.com>
2021-11-28 06:01:02 -0500

    Beginning to look into the userProfile

--- Profile/userprofile.php
+++ Profile/userprofile.php
@@ -277,8 +277,7 @@ if (mysqli_num_rows($result) > 0) {
                         <span><?php echo $row['username'] ?></span>
                     </div>
                     <div class="userJobs">
-
-                        <?php
+                    <?php
                             if($fetchJobCount['jobCount'] < 1){
                         ?>
                         <p>
@@ -303,62 +302,8 @@ if (mysqli_num_rows($result) > 0) {
                             
                             if (mysqli_num_rows($jobResult) > 0) {
                                 while ($r = mysqli_fetch_assoc($jobResult)) {
-                            ?>
-                            <div class="jobPost" onclick="location.href=`../newPostJob/job.php?id=<?php echo $r['id']; ?>`">
-                                <div class="job-title">
-                                    <a href="../newPostJob/job.php?id=<?php echo $r['id']; ?>"><?php echo $r['title']; ?></a>
-                                </div>
+                            ?>   
 
-                                <!-- ----------------------------------------- -->
-                                <!-- JOB STATUS -->
-                                <!-- 0 : OPEN -->
-                                <!-- 1 : CLOSED -->
-                                <!-- -1 : IN PROGRESS -->
-                                <!-- ----------------------------------------- -->
-
-                                <div class="status">
-                                    <p>Status:</p>
-                                    <span>
-                                        <?php if ($r['status'] == 0) { ?>
-                                            <green><?php echo "Open"; ?></green>
-                                        <?php } else if ($r['status'] == 1) { ?>
-                                            <red><?php echo "Closed"; ?></red>
-                                        <?php } else if ($r['status'] == -1) { ?>
-                                            <yellow><?php echo "In-Progress"; ?></yellow>
-                                        <?php } ?>
-                                    </span>
-                                </div>
-                                <div class="card1">
-                                    <div class="postedOn">
-                                        <p>Posted on:</p>
-                                        <span>
-                                            <?php echo $r['datePosted']; ?>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div class="card2">
-                                    <div class="location">
-                                        <p>Location: </p>
-                                        <span> <?php echo ucfirst($r['location']); ?></span>
-                                    </div>
-
-                                    <div class="price">
-                                        <p>Pay: </p>
-                                        <?php if ($r['rate'] > 0) { ?>
-                                            <span>$<?php echo $r['rate']; ?> / hr - </span>
-                                            <span><?php echo "Hourly Rate" ?></span>
-                                        <?php } else if ($r['budget'] > 0) { ?>
-                                            <span>$ <?php echo $r['budget']; ?> - </span>
-                                            <span><?php echo "Project Budget" ?></span>
-                                        <?php } else { ?>
-                                            <p></p>
-                                            <span> <?php echo "No budget or pay rate set yet..."; ?></span>
-                                        <?php } ?>
-                                    </div>
-                                </div>
-
-                            </div>     
                             <?php
                                 }
                             }
