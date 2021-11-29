c061f47238b4bf941f926e7ef2114b5f38c95a42
Your Name <leobarrientos02@gmail.com>
2021-11-29 04:53:33 -0500

    Beginning to look into the userProfile

--- Profile/userprofile.php
+++ Profile/userprofile.php
@@ -141,7 +141,7 @@ if (mysqli_num_rows($result) > 0) {
                                 if ($row['freelancer_id'] != NULL) {
                             ?>
                             <div class="freelancerQoute">
-                                <p>**Freelancers also have the option to post jobs to the EZWork marketplace.</p>
+                                <p>**Freelancers also have the option to post jobs to the <span>EZWORK</span>cmarketplace.</p>
                             </div>
                             <?php
                                 }
@@ -276,6 +276,96 @@ if (mysqli_num_rows($result) > 0) {
                     <div class="ratings">
                         <h4>Reviews</h4>
                     </div>
+                    <div class="userJobs">
+
+                        <?php
+                            if($fetchJobCount['jobCount'] < 1){
+                        ?>
+                        <p>
+                            <?php echo "Seems like " . $row['username'] . " is inactive."; ?>
+                        </p>
+                        <img src="../Image/sad-cartoon.gif" alt="no-activity">
+                        <?php   
+                            }
+                            else{
+                        ?>     
+                            <h3>All Jobs Post(s)</h3>
+                            <?php
+                            $username = $row['username'];
+                            $getUserID = "SELECT id FROM clients WHERE username = '$username'";
+                            $getResult = mysqli_query($conn, $getUserID);
+                            $userrow = mysqli_fetch_assoc($getResult);
+                            $userID = $userrow['id'];
+                            $jobSQL = "SELECT * FROM jobs WHERE user_id='$userID' ORDER BY id DESC";
+                            $jobResult = mysqli_query($conn, $jobSQL) or die(mysqli_errno($conn));  
+                            
+                            if (mysqli_num_rows($jobResult) > 0) {
+                                while ($r = mysqli_fetch_assoc($jobResult)) {
+                            ?>
+                            <div class="jobPost" onclick="location.href=`../newPostJob/job.php?id=<?php echo $r['id']; ?>`">
+                                <div class="job-title">
+                                    <a href="../newPostJob/job.php?id=<?php echo $r['id']; ?>"><?php echo $r['title']; ?></a>
+                                </div>
+
+                                <!-- ----------------------------------------- -->
+                                <!-- JOB STATUS -->
+                                <!-- 0 : OPEN -->
+                                <!-- 1 : CLOSED -->
+                                <!-- -1 : IN PROGRESS -->
+                                <!-- ----------------------------------------- -->
+
+                                <div class="card1">
+                                    <div class="status">
+                                        <p>Status:</p>
+                                        <span>
+                                            <?php if ($r['status'] == 0) { ?>
+                                                <green><?php echo "Open"; ?></green>
+                                            <?php } else if ($r['status'] == 1) { ?>
+                                                <red><?php echo "Closed"; ?></red>
+                                            <?php } else if ($r['status'] == -1) { ?>
+                                                <yellow><?php echo "In-Progress"; ?></yellow>
+                                            <?php } ?>
+                                        </span>
+                                    </div>
+                                    <div class="postedOn">
+                                        <p>Posted on:</p>
+                                        <span>
+                                            <?php echo $r['datePosted']; ?>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="card2">
+                                    <div class="location">
+                                        <p>Location: </p>
+                                        <span> <?php echo ucfirst($r['location']); ?></span>
+                                    </div>
+
+                                    <div class="price">
+                                        <p>Pay: </p>
+                                        <?php if ($r['rate'] > 0) { ?>
+                                            <span>$<?php echo $r['rate']; ?> / hr - </span>
+                                            <span><?php echo "Hourly Rate" ?></span>
+                                        <?php } else if ($r['budget'] > 0) { ?>
+                                            <span>$ <?php echo $r['budget']; ?> - </span>
+                                            <span><?php echo "Project Budget" ?></span>
+                                        <?php } else { ?>
+                                            <p></p>
+                                            <span> <?php echo "No budget or pay rate set yet..."; ?></span>
+                                        <?php } ?>
+                                    </div>
+                                </div>
+
+                            </div>                        
+                            <?php
+                                }
+                            }
+                            ?> 
+                        <?php   
+                            }
+                        ?>
+
+                    </div>                  
                 </div>
                 <?php
                 } else{
