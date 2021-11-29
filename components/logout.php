93e6b09b9643f4f5f5ed706c4d2b9beb300b6d85
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-11-29 09:03:27 -0500

    update

--- components/logout.php
+++ components/logout.php
@@ -2,6 +2,5 @@
 session_start();
 unset($_SESSION['user_id']);
 unset($_SESSION['userid']);
-
-$_SESSION['logoutSuccess'] = "Logged out successfully";
+session_destroy();
 header('location:../login/index');
