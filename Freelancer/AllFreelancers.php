8ce467bfc2fbc8e934c142cc412a1532fcb57122
Your Name <leobarrientos02@gmail.com>
2021-12-03 04:55:29 -0500

    All Freelancers page

--- Freelancer/AllFreelancers.php
+++ Freelancer/AllFreelancers.php
@@ -67,6 +67,8 @@ $userID = $_SESSION['user_id'];
                                     $data = mysqli_query($conn, $dataSQL) or die(mysqli_errno($conn));
                                     if (mysqli_num_rows($data) > 0) {
                                         while ($dataArray = mysqli_fetch_assoc($data)) {
+                                            if ($row['username'] != $_SESSION['userid']) {
+                                                if ($row['freelancer_id'] != NULL) {
                                     ?>
                                 <h3>
                                     <?php echo $dataArray['expertise']; ?>                                
@@ -75,6 +77,8 @@ $userID = $_SESSION['user_id'];
                                 <h5><?php echo $dataArray['country']; ?></h5>
 
                                     <?php
+                                                }
+                                            }
                                         }
                                     }
                                     ?>
