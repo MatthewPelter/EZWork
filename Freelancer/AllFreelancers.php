c578e8ca06d61a0429361a08cdfac23d05c8278d
Your Name <leobarrientos02@gmail.com>
2021-12-04 04:09:14 -0500

    All Freelancers page

--- Freelancer/AllFreelancers.php
+++ Freelancer/AllFreelancers.php
@@ -53,35 +53,48 @@ $userID = $_SESSION['user_id'];
 
                                 if ($row['freelancer_id'] != NULL) {
                     ?>
-                     
-                    <a href="../Profile/userprofile.php?name=<?php echo $row['username']; ?>">
-                        <div class="freelancerCard" onclick="location.href=`" >
-                            <div class="freelancerImg">
-                                <img src="<?php echo $row['avatar']; ?>" alt=`<?php echo $row['username']; ?>`>
-                            </div>
-                            <div class="freelancerInfo">
-                                <h2><?php echo $row['username']; ?></h2>
-                            
                                 <?php
-                                        $freeID1 = $row['freelancer_id'];
-                                        $pullExpertise = mysqli_query($conn, "SELECT expertise AS skill FROM freelancers"); 
-                                        $data = mysqli_fetch_assoc($pullExpertise);    
-                                ?>                                
-                                <h3>
-                                    <?php echo $data['skill']; ?>                                
-                                </h3>
-                                <h4>$ <span>10</span> per hour</h4>
-                                <h5>China</h5>
+                                    $dataSQL = "SELECT * FROM freelancers";
+                                    $dataResult = mysqli_query($conn, $dataSQL) or die(mysqli_errno($conn));
 
-                                <?php
-                                $freeID = $row['freelancer_id'];
-                                $pullJobs = mysqli_query($conn, "SELECT COUNT(*) AS completedJobs FROM jobs WHERE freelancer_id = '$freeID' AND status=1");
-                                $pullJobCount = mysqli_fetch_assoc($pullJobs);                            
-                                ?>
-                                <p><?php echo $pullJobCount['completedJobs']; ?> jobs completed</p>
-                            </div>
-                        </div>
-                    </a>
+                                    if (mysqli_num_rows($dataResult) > 0) {
+                                        while ($r = mysqli_fetch_assoc($dataResult)){
+                                ?>            
+                                            <a href="../Profile/userprofile.php?name=<?php echo $row['username']; ?>">
+                                                <div class="freelancerCard" onclick="location.href=`" >
+                                                    <div class="freelancerImg">
+                                                        <img src="<?php echo $row['avatar']; ?>" alt=`<?php echo $row['username']; ?>`>
+                                                    </div>
+                                                    <div class="freelancerInfo">
+                                                        <h2><?php echo $row['username']; ?></h2>
+                                                        <h3>
+                                                            <?php echo $r['expertise']; ?>                            
+                                                        </h3>
+                                                        <h4>$ 
+                                                            <span>
+                                                                <?php echo $r['hourRate']; ?>   
+                                                            </span>
+                                                             per hour
+                                                        </h4>
+                                                        
+                                                        <h5>
+                                                            <?php echo $r['country']; ?>   
+                                                        </h5>
+                        
+                                                        <?php
+                                                        $freeID = $row['freelancer_id'];
+                                                        $pullJobs = mysqli_query($conn, "SELECT COUNT(*) AS completedJobs FROM jobs WHERE freelancer_id = '$freeID' AND status=1");
+                                                        $pullJobCount = mysqli_fetch_assoc($pullJobs);                            
+                                                        ?>
+                                                        <p><?php echo $pullJobCount['completedJobs']; ?> jobs completed</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                <?php            
+                                        }
+                                    }
+                                ?>                     
+
 
 
 
