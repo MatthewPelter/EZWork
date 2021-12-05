761358a1f0b93f316e8c18a5737cb311722a7e2c
Your Name <leobarrientos02@gmail.com>
2021-12-05 04:11:30 -0500

    myHires Page

--- Freelancer/myHires.php
+++ Freelancer/myHires.php
@@ -11,15 +11,42 @@ $userID = $_SESSION['user_id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
+            <title>EZWork | My Hires</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet"> 
+            <link rel="icon" href="../logo/logo.svg">
+            <link rel="stylesheet" href="../Styles/style.css">
+        </head>
+    </head>
 <body>
 
-<?php
+    <!-- NAV BAR -->
+    <?php include '../navbar.php'; ?>
+    <!-- END NAVBAR -->
+
+    <div class="AllFreelancers">
+
+        <div class="AllFreelancersHeader">
+            <h2>My Hires</h2>
+            <ul>
+                <li><a href="../ClientProfile/index.php">My Profile</a></li>
+                <li>/</li>
+                <li>My Hires</li>
+            </ul>
+        </div>
+        <div class="AllFreelancersContainer">
+        <?php
                 if (mysqli_num_rows($jobResult) > 0) {
                     while ($r = mysqli_fetch_assoc($jobResult)) {
 ?>
@@ -27,8 +54,21 @@ $userID = $_SESSION['user_id'];
 <?php
                     }
                 }
-?>
+?>              
+        </div>
+    </div>    
+
+    <!-- Footer -->
+    <?php include '../footer.php'; ?>
+    <!-- END Footer -->
+
+        <!--DataList-->
+        <datalist id="allskills">
+        
+        </datalist>
 
     
 </body>
+<!--Script for the search bar and datalist-->
+<script src="../SkillsContainer/searchProfile.js"></script>
 </html>
