08b921fb282c6dfd34cb946dfccdbf5210400fe7
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-12-08 04:28:00 -0500

    update

--- newPostJob/offerJob.php
+++ newPostJob/offerJob.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id'])) {
     die("NOT LOGGED IN");
 } else {
     $user_id = $_SESSION['user_id'];
-    $sql = "SELECT * FROM clients WHERE id = '$user_id' limit 1";
+    $sql = "SELECT * FROM clients WHERE id = '$user_id' LIMIT 1";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) == 0) {
         header('Location: ../login/index');
@@ -20,17 +20,11 @@ $job_id = $_GET['id'];
 $job_id = mysqli_real_escape_string($conn, $job_id);
 $job_id = htmlspecialchars($job_id);
 
-$jobSQL = "SELECT jobs.*, clients.avatar FROM jobs INNER JOIN clients ON jobs.user_id = clients.id WHERE jobs.id='$job_id' LIMIT 1";
+$jobSQL = "SELECT * FROM offerjobs WHERE id='$job_id' LIMIT 1";
 $jobResult = mysqli_query($conn, $jobSQL);
 
 if (mysqli_num_rows($jobResult) > 0) {
     $r = mysqli_fetch_assoc($jobResult);
-
-    $uid = $r['user_id'];
-    $unameSQL = "SELECT username FROM clients WHERE id='$uid'";
-    $unameResult = mysqli_query($conn, $unameSQL);
-    $unameFetched = mysqli_fetch_assoc($unameResult);
-    $receiverUsername = $unameFetched['username'];
 } else {
     header("location: ./jobs.php");
 }
