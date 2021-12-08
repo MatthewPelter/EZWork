df8285135bded973190f7b0d8eb74aaadd25e448
Your Name <leobarrientos02@gmail.com>
2021-12-08 14:49:46 -0500

    trying a new design for the job progress page

--- newPostJob/job.php
+++ newPostJob/job.php
@@ -401,7 +401,130 @@ $getFreelancerID = $getFreelancerID['freelancer_id'];
 
     if ($r['status'] == -1 && ($r['user_id'] == $user_id || $r['freelancer_id'] == $getFreelancerID)) { ?>
         <!-- style as you please. just temporary -->
-        <div class="job progress">
+        <div class="job-progress-section">
+            <h2>Job Progress</h2>
+            <div class="job-progress-container">
+                <div class="job-progress-header">
+                    <h2>
+                        <?php echo $r['title']?>
+                    </h2>
+                    <p>Status:
+                            <?php if ($r['status'] == 0) { ?>
+                                <span style="color: lightgreen;font-weight: bolder;"><?php echo "Open"; ?></span>
+                            <?php } else if ($r['status'] == 1) { ?>
+                                <span style="color: red;font-weight: bolder;"><?php echo "Closed"; ?></span>
+                            <?php } else { ?>
+                                <span style="color: royalblue;font-weight: bolder;"><?php echo "In-Progress"; ?></span>
+                            <?php } ?>
+                    </p>                
+                </div>
+
+                <div class="job-progress-status">
+                    <div class="jobDescription">
+                        <div class="wrapper">
+    
+                            <h1>Project Progress</h1>
+    
+                            <ol class="ProgressBar">
+                                <li class="ProgressBar-step">
+                                    <svg class="ProgressBar-icon">
+                                        <use xlink:href="#checkmark-bold" />
+                                    </svg>
+                                    <span class="ProgressBar-stepLabel">Accepted Job</span>
+                                </li>
+                                <li class="ProgressBar-step">
+                                    <svg class="ProgressBar-icon">
+                                        <use xlink:href="#checkmark-bold" />
+                                    </svg>
+                                    <span class="ProgressBar-stepLabel">Started Work</span>
+                                </li>
+                                <li class="ProgressBar-step">
+                                    <svg class="ProgressBar-icon">
+                                        <use xlink:href="#checkmark-bold" />
+                                    </svg>
+                                    <span class="ProgressBar-stepLabel">Finished</span>
+                                </li>
+                            </ol>
+                        </div>
+    
+                        <svg xmlns="http://www.w3.org/2000/svg">
+                            <symbol id="checkmark-bold" viewBox="0 0 24 24">
+                                <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
+                            </symbol>
+                        </svg>
+                    </div>
+    
+                    <?php
+                    $workFreelancer = $r['freelancer_id'];
+                    $getFreelancerName = mysqli_query($conn, "SELECT id, username, avatar FROM clients WHERE freelancer_id='$workFreelancer'");
+                    $getFreelancerName = mysqli_fetch_assoc($getFreelancerName);
+    
+                    $freelancerUserID = $getFreelancerName['id'];
+                    ?>
+    
+                    <div class="options">
+                        <?php if ($r['user_id'] == $_SESSION['user_id']) {
+                            if ($r['paid'] == 0) { ?>
+                                <button class="pay">
+                                    <i class="fa fa-money" aria-hidden="true"></i>
+                                    Pay For Service
+                                </button>
+                            <?php } else if ($r['status'] != 1 && $r['freelancer_complete'] == 1) {
+                            ?>
+    
+                                <h2>The freelancer has compelted your job.</h2>
+                                <h3>If you think they completed it correctly, mark it as complete.</h3>
+                                <button class="completeClient">
+                                    <i class="fa fa-flag" aria-hidden="true"></i>
+                                    Mark Job as Complete
+                                </button>
+                            <?php } else { ?>
+                                <h2>Freelancer is working on your job now.</h2>
+                        <?php
+                            }
+                        } ?>
+                        <?php if ($r['freelancer_id'] == $getFreelancerID) {
+                            if ($r['status'] != 1 && $r['freelancer_complete'] == 0) {
+                                if ($r['paid'] == 0) { ?>
+                                    <h2>Wait until the client pays before you begin working.</h2>
+                                <?php
+                                } else {
+                                ?>
+                                    <button class="completeFreelancer">
+                                        <i class="fa fa-flag" aria-hidden="true"></i>
+                                        Mark Job as Complete
+                                    </button>
+                        <?php }
+                            }
+                        } ?>
+                    </div>
+                </div>
+
+                <div class="job-poster-info">
+                    <div class="clientInfo">
+                        <h3>About the Freelancer</h3>
+                        <div class="username">
+                            <p>Work By: </p>
+    
+                            <a href="../Profile/userprofile.php?name=<?php echo $getFreelancerName['username']; ?>"><?php echo $getFreelancerName['username']; ?></a>
+                        </div>
+                        <div class="img-card">
+                            <img src="<?php echo $getFreelancerName['avatar']; ?>" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="job-chat">
+                    <div class="messageChat">
+                        <!-- messages loaded from jquery -->
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        
+        <!--<div class="job progress">
             <h2>Job Progress</h2>
             <div class="job-container">
                 <div class="jobCard">
@@ -412,6 +535,7 @@ $getFreelancerID = $getFreelancerID['freelancer_id'];
                         <!-- 1 : CLOSED -->
                         <!-- -1 : IN PROGRESS -->
                         <!-- ----------------------------------------- -->
+                        <!--
                         <p>Status:
                             <span id="status">
                                 <?php if ($r['status'] == 1) {
@@ -426,6 +550,8 @@ $getFreelancerID = $getFreelancerID['freelancer_id'];
                 </div>
                 <div class="messageChat" style="grid-area: 1/1/4/1;">
                     <!-- messages loaded from jquery -->
+                            -->
+                <!--            
                 </div>
 
                 <div class="jobDescription">
@@ -519,6 +645,7 @@ $getFreelancerID = $getFreelancerID['freelancer_id'];
                 </div>
             </div>
         </div>
+        -->
     <?php } ?>
 
 
