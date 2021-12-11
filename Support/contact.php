f3d636ffe6443bc900ecdc5fdd8c4128b616ac0c
Your Name <leobarrientos02@gmail.com>
2021-12-11 04:58:22 -0500

    Made a services section

--- Support/contact.php
+++ Support/contact.php
@@ -32,7 +32,19 @@ require_once('../classes/DB.php');
         </div>
 
         <div class="contactContainer">
-            
+            <div class="email">
+
+            </div>
+            <div class="phone">
+
+            </div>
+            <div class="supportTeam">
+
+            </div>
+
+            <div class="location">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.4138865725595!2d-73.42888748540187!3d40.7529207430495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e82afe99b445e9%3A0x6c53280083cbf6be!2sFarmingdale%20State%20College!5e0!3m2!1sen!2sus!4v1639216553053!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
         </div>
     </div>
     <?php include '../footer.php'; ?>
