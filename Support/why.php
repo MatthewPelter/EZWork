85460e4af889dcb6ecdaa645b2d95c699d585c9b
Your Name <leobarrientos02@gmail.com>
2021-12-11 21:26:07 -0500

    whyEZwork page

--- Support/why.php
+++ Support/why.php
@@ -34,26 +34,30 @@ require_once('../classes/DB.php');
         <div class="whyEZWorkContainer">
             <div class="mission">
                 <h3>Our Mission</h3>
-                <i class="fas fa-chevron-down"></i>
-            </div>    
-            <div class="missionCard">
-                <p>
-                    &nbsp;Today, it is difficult finding business as a solo entrepreneur. It’s also just as challenging to find people to perform needed work. EZWork believes that this is because of oversaturation of job posting sites, as well as lack of opportunities. EZWork wants people to earn a steady income finding work through our site, and also wants the public to hire talented people for their own services. The site allows direct communication between both people needing services and those offering services, and allows for profile and job descriptions, as well as direct payment to the individual after the work has been completed.
-                </p>
-                
-                <p>
-                    &nbsp;EZWork has decided to work on this problem of matching those looking for work with those needing services. There’s so much undiscovered and unappreciated talent in the world. There are creators on websites like YouTube, Instagram, and Twitter that try to get recognized for their talents, but there’s too many of them. The site will serve to give these creators and innovators an opportunity. They’ll be able to be hired for their work. This won’t be limited to online things; skilled laborers will also find that our website is friendly towards them. Handymen can find a steady number of jobs that will keep them busy. This site have user profiles, and users can post their skills. Others looking to hire can also post what they are looking for.
-                </p>
+                <i id="missionUp" class="fas fa-chevron-up"></i>
+            </div>
+            <div class="missionWrapper">
+                <div class="missionCard">
+                    <p>
+                        &nbsp;Today, it is difficult finding business as a solo entrepreneur. It’s also just as challenging to find people to perform needed work. EZWork believes that this is because of oversaturation of job posting sites, as well as lack of opportunities. EZWork wants people to earn a steady income finding work through our site, and also wants the public to hire talented people for their own services. The site allows direct communication between both people needing services and those offering services, and allows for profile and job descriptions, as well as direct payment to the individual after the work has been completed.
+                    </p>
+                    
+                    <p>
+                        &nbsp;EZWork has decided to work on this problem of matching those looking for work with those needing services. There’s so much undiscovered and unappreciated talent in the world. There are creators on websites like YouTube, Instagram, and Twitter that try to get recognized for their talents, but there’s too many of them. The site will serve to give these creators and innovators an opportunity. They’ll be able to be hired for their work. This won’t be limited to online things; skilled laborers will also find that our website is friendly towards them. Handymen can find a steady number of jobs that will keep them busy. This site have user profiles, and users can post their skills. Others looking to hire can also post what they are looking for.
+                    </p>
+                </div>
             </div>
 
             <div class="whatIsEZWork">
                 <h3>What is EZWork?</h3>
                 <i class="fas fa-chevron-down"></i>
             </div>
-            <div class="whatCard">
-                <p>
-                    &nbsp;The web application EZWork was created to provide a platform for skilled workers and talented people to freelance, and easily find projects to work on as an extra way to earn income. We found that consumers go to big companies like Home Depot, Best Buy’s geek squad because there is a dedicated place/platform to find skilled workers. Big corporations like the one previously mentioned take a high cut of what a freelancer worker would get. Now we bring the option of hiring skilled freelancers, or the people that work at geek squad to the consumers/employers, not only does consumers benefit from paying less, but the worker also takes the full payout (minus our small service fee) for their hard work!
-                </p>
+            <div class="whatWrapper">
+                <div class="whatCard">
+                    <p>
+                        &nbsp;The web application EZWork was created to provide a platform for skilled workers and talented people to freelance, and easily find projects to work on as an extra way to earn income. We found that consumers go to big companies like Home Depot, Best Buy’s geek squad because there is a dedicated place/platform to find skilled workers. Big corporations like the one previously mentioned take a high cut of what a freelancer worker would get. Now we bring the option of hiring skilled freelancers, or the people that work at geek squad to the consumers/employers, not only does consumers benefit from paying less, but the worker also takes the full payout (minus our small service fee) for their hard work!
+                    </p>
+                </div>
             </div>
 
             <div class="community">
@@ -84,6 +88,24 @@ require_once('../classes/DB.php');
 </body>
 
 <!--Script for the search bar and datalist-->
-<scrip src="../SkillsContainer/allJobsSkills.js"></script>
+<script src="../SkillsContainer/allJobsSkills.js"></script>
+
+<script>
+    const missionArrowUp = document.getElementById('missionUp');
+
+    missionArrowUp.addEventListener('click',()=>{
+        var missionWrapper = document.querySelector('.missionWrapper');
+
+        if(getComputedStyle(missionWrapper).display === 'block'){
+            missionWrapper.style.display = 'none';
+            missionArrowUp.style.transform = "rotate(180deg)";
+        }else{
+            missionWrapper.style.display = 'block';
+            missionArrowUp.style.transform = "rotate(360deg)";
+        }
+
+    });
+
+</script>
 
 </html>
