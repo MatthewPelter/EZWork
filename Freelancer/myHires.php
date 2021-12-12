83165210368b09693a7e180937f7eb8d3393ed7b
MatthewPelter <70623889+MatthewPelter@users.noreply.github.com>
2021-12-12 00:36:26 -0500

    update

--- Freelancer/myHires.php
+++ Freelancer/myHires.php
@@ -6,29 +6,32 @@ require_once("../classes/DB.php");
 $username = $_SESSION['userid'];
 $userID = $_SESSION['user_id'];
 
-    $sql = "SELECT * FROM jobs WHERE user_id='$userID' AND freelancer_id <> ''";
-    $jobResult = mysqli_query($conn, $sql) or die(mysqli_errno($conn));
+$sql = "SELECT * FROM jobs WHERE user_id='$userID' AND freelancer_id <> ''";
+$jobResult = mysqli_query($conn, $sql) or die(mysqli_errno($conn));
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+
     <head>
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
-            <title>EZWork | My Hires</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet"> 
-            <link rel="icon" href="../logo/logo.svg">
-            <link rel="stylesheet" href="../Styles/style.css">
-        </head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="A platform for skilled workers or talented people to freelance, find projects to work on, extra ways to earn income.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/e9089fea9d.js" crossorigin="anonymous"></script>
+        <title>EZWork | My Hires</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+        <link rel="icon" href="../logo/logo.svg">
+        <link rel="stylesheet" href="../Styles/style.css">
     </head>
+</head>
+
 <body>
 
     <!-- NAV BAR -->
@@ -46,41 +49,41 @@ $userID = $_SESSION['user_id'];
             </ul>
         </div>
         <?php
-                if( mysqli_num_rows($jobResult) == 0){
+        if (mysqli_num_rows($jobResult) == 0) {
         ?>
-                <div class="noHired">
-                    <span>Seems like you have not hired anyone for some reason.</span>
-                    <img src="../Image/sad-cartoon.gif" alt="no-activity">
-                </div>
-               
+            <div class="noHired">
+                <span>Seems like you have not hired anyone for some reason.</span>
+                <img src="../Image/sad-cartoon.gif" alt="no-activity">
+            </div>
+
         <?php
-                }
-        ?>        
+        }
+        ?>
         <div class="myHiredContainer">
-        <?php
+            <?php
 
-                if (mysqli_num_rows($jobResult) > 0) {
-                    while ($r = mysqli_fetch_assoc($jobResult)) {
-        ?>
+            if (mysqli_num_rows($jobResult) > 0) {
+                while ($r = mysqli_fetch_assoc($jobResult)) {
+            ?>
                     <?php
-                        $freeID = $r['freelancer_id'];
-                        $getFreelancersSQL = "SELECT * from clients WHERE freelancer_id='$freeID'";
-                        $freelancers = mysqli_query($conn, $getFreelancersSQL) or die(mysqli_errno($conn));
-                    
-                        if (mysqli_num_rows($freelancers) > 0) {
+                    $freeID = $r['freelancer_id'];
+                    $getFreelancersSQL = "SELECT DISTINCT * from clients WHERE freelancer_id='$freeID'";
+                    $freelancers = mysqli_query($conn, $getFreelancersSQL) or die(mysqli_errno($conn));
 
-                            while ($f = mysqli_fetch_assoc($freelancers)) {
-                                
+                    if (mysqli_num_rows($freelancers) > 0) {
+
+                        while ($f = mysqli_fetch_assoc($freelancers)) {
+
                     ?>
-                    <a href="../Profile/userprofile.php?name=<?php echo $f['username']; ?>">
-                        <div class="freelancerCard">
-                            <div class="freelancerImg">
-                                <img src="<?php echo $f['avatar']; ?>" alt=`<?php echo $f['username']; ?>`>
-                            </div>
-                            <div class="freelancerInfo">
-                                <h2><?php echo $f['username']; ?></h2>
-                                
-                                <!-- Couldn't get the data such as expertise for each freelacner
+                            <a href="../Profile/userprofile.php?name=<?php echo $f['username']; ?>">
+                                <div class="freelancerCard">
+                                    <div class="freelancerImg">
+                                        <img src="<?php echo $f['avatar']; ?>" alt=`<?php echo $f['username']; ?>`>
+                                    </div>
+                                    <div class="freelancerInfo">
+                                        <h2><?php echo $f['username']; ?></h2>
+
+                                        <!-- Couldn't get the data such as expertise for each freelacner
                                 <h3>
                                     Software Developer                                
                                 </h3>
@@ -95,38 +98,39 @@ $userID = $_SESSION['user_id'];
                                    
                                 </h5>
                                 -->
-                                <?php
-                                    $freeID = $f['freelancer_id'];
-                                    $pullJobs = mysqli_query($conn, "SELECT COUNT(*) AS completedJobs FROM jobs WHERE freelancer_id = '$freeID' AND status=1");
-                                    $pullJobCount = mysqli_fetch_assoc($pullJobs);                            
-                                ?>
-                                <p><?php echo $pullJobCount['completedJobs']; ?> jobs completed</p>
-                            </div>
-                        </div>
-                    </a>
+                                        <?php
+                                        $freeID = $f['freelancer_id'];
+                                        $pullJobs = mysqli_query($conn, "SELECT COUNT(*) AS completedJobs FROM jobs WHERE freelancer_id = '$freeID' AND status=1");
+                                        $pullJobCount = mysqli_fetch_assoc($pullJobs);
+                                        ?>
+                                        <p><?php echo $pullJobCount['completedJobs']; ?> jobs completed</p>
+                                    </div>
+                                </div>
+                            </a>
                     <?php
-                            }
                         }
-                    ?>
-                                  
-<?php
                     }
+                    ?>
+
+            <?php
                 }
-?>              
+            }
+            ?>
         </div>
-    </div>    
+    </div>
 
     <!-- Footer -->
     <?php include '../footer.php'; ?>
     <!-- END Footer -->
 
-        <!--DataList-->
-        <datalist id="allskills">
-        
-        </datalist>
+    <!--DataList-->
+    <datalist id="allskills">
 
-    
+    </datalist>
+
+
 </body>
 <!--Script for the search bar and datalist-->
 <script src="../SkillsContainer/searchProfile.js"></script>
+
 </html>
