f460bf2386ad7a6a38f73a14037565437c5522bd
Your Name <leobarrientos02@gmail.com>
2021-12-13 16:10:17 -0500

    trying to add desing to offerPage

--- newPostJob/offerJob.php
+++ newPostJob/offerJob.php
@@ -228,16 +228,16 @@ if (mysqli_num_rows($jobResult) > 0) {
                     </li>
                 </ol>
             </div>
+
+            <div class="messageChat" style="margin-bottom: 1rem;">
+                <!-- insert message -->
+            </div>            
         
             <svg xmlns="http://www.w3.org/2000/svg">
                 <symbol id="checkmark-bold" viewBox="0 0 24 24">
                     <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
                 </symbol>
             </svg>
-
-            <div class="messageChat" style="margin-bottom: 1rem;">
-                <!-- insert message -->
-            </div>
 
         </div>
 
