8420c832424965327e626147504ba5520c784e0b
Your Name <leobarrientos02@gmail.com>
2021-12-13 20:21:38 -0500

    trying to add desing to offerPage

--- Support/help.php
+++ Support/help.php
@@ -38,7 +38,7 @@ require_once('../classes/DB.php');
             </div>
             <div class="loginOutWrapper">
                 <div class="loginOutCard">
-                    <video autoplay muted loop id="loginOutVid" width="50vw" height="50vh" controls>
+                    <video autoplay muted loop id="loginOutVid" width="20rem" height="30rem" controls>
                         <source src="../vid/login.mp4" type="video/mp4">
                     </video>
                 </div>
